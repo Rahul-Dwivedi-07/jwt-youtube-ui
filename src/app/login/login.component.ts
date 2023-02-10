5b0a391f4dd67ce7948df671bed27d62ff8c1c0f
Rahul-Dwivedi-07 <87537782+Rahul-Dwivedi-07@users.noreply.github.com>
2023-02-10 18:36:06 +0530

    Add files via upload

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserAuthService } from '../_services/user-auth.service';
+import { UserService } from '../_services/user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  constructor(
+    private userService: UserService,
+    private userAuthService: UserAuthService,
+    //for routing
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
+
+  login(loginForm: NgForm) {
+    this.userService.login(loginForm.value).subscribe(
+      (response: any) => {
+        this.userAuthService.setRoles(response.user.role);
+        this.userAuthService.setToken(response.jwtToken);
+        
+        //role array
+        //[0] is as per user_id and role_id table
+        //roleName exactly same as on the console
+        const role = response.user.role[0].roleName;
+        if (role === 'Admin') {
+          this.router.navigate(['/admin']);
+        } else {
+          this.router.navigate(['/user']);
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+}
